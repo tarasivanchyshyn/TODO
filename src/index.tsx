31f94d92fef070c247c5cf436c1ca06d7228701e
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-09-20 17:57:44 +0300

    initialized redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import store from './store/store';
 import App from './App';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +12,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   // </React.StrictMode>
 );
