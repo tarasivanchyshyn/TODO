8f47c8c06f945264dfad6002b898d99be8fe1669
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-11-08 22:16:24 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build/static')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(
-        __dirname,
-        '../',
-        'frontend',
-        'build',
-        'static',
-        'index.html'
-      )
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
   );
 } else {
