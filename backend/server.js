9bc225905eee4adfffe0d326155b96828ff3ca18
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-11-08 21:43:41 +0200

    fixes

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build/static')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(
+        __dirname,
+        '../',
+        'frontend',
+        'build',
+        'static',
+        'index.html'
+      )
     )
   );
 } else {
-  app.get('/', (req, res) => res.send('Fuck off'));
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
