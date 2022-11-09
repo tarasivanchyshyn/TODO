1d4adbf20875326eaf42a82cb93d36c8d89e9057
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-11-09 11:26:26 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,17 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
+  // app.get('*', (req, res) =>
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+  //   )
+  // );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
+
+app.get('/test', (req, res) => {
+  res.send('It works');
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
