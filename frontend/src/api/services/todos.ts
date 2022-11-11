6d679a174889f8cbf0297f4275f54a5ed9df4a7f
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-11-11 12:52:39 +0200

    fixes

--- frontend/src/api/services/todos.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// export const getAllTodos = () =>
-//   axios.get('https://todos-h97u.onrender.com/api/todos');
-
-export const getAllTodos = async () => {
-  const res = await axios.get('https://todos-h97u.onrender.com/api/todos');
-  return res.data;
-};
