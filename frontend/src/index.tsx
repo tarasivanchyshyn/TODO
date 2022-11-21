dbb1c6b6d92fee740adc35ed3e62ea2f83d74772
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-11-21 11:07:23 +0200

    fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import store, { persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
