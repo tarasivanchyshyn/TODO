709059181c4ca7b080e05f5ecab35bb616457d9d
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-12-08 11:13:56 +0200

    added interceptor logout

--- frontend/src/api/services/axios.ts
+++ frontend/src/api/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import { serverBaseURL } from '../../constants';
-import { refresh } from '../../store/authSlice';
+import { logout, refresh } from '../../store/authSlice';
 import store from '../../store/store';
 
 const instance = axios.create({
@@ -23,19 +23,22 @@ instance.interceptors.response.use(
     if (error.response.status === 401) {
       const originalRequest = error.config;
       const user = store.getState().auth.user;
-
-      if (user) {
-        const newTokens = await store.dispatch(
-          refresh({
-            access_token: user.accessToken,
-            refresh_token: user.refreshToken
-          })
-        );
-        originalRequest.headers = {
-          ...originalRequest.headers,
-          Authorization: `Bearer ${newTokens.payload.accessToken}`
-        };
-        return instance.request(originalRequest);
+      try {
+        if (user) {
+          const newTokens = await store.dispatch(
+            refresh({
+              access_token: user.accessToken,
+              refresh_token: user.refreshToken
+            })
+          );
+          originalRequest.headers = {
+            ...originalRequest.headers,
+            Authorization: `Bearer ${newTokens.payload.accessToken}`
+          };
+          return instance.request(originalRequest);
+        }
+      } catch {
+        store.dispatch(logout());
       }
     } else {
       return Promise.reject(error);
