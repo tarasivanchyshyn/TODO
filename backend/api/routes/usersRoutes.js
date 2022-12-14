8e818ec46cc1fdd6f245f5485ecf91a0d536f11c
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-12-14 12:09:30 +0200

    finished

--- backend/api/routes/usersRoutes.js
+++ backend/api/routes/usersRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { loginUser, refreshToken } from '../controllers/usersController.js';
+import {
+  registerUser,
+  loginUser,
+  refreshToken
+} from '../controllers/usersController.js';
 
 const router = express.Router();
 
+router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/refresh', refreshToken);
 
